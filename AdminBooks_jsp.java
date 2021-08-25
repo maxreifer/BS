65470cc3c177dcd325812393a4d2ebf745961dcb
maxreifer <88498079+maxreifer@users.noreply.github.com>
2021-08-25 12:01:13 +0300

    Update AdminBooks_jsp.java

--- AdminBooks_jsp.java
+++ AdminBooks_jsp.java
@@ -3,7 +3,7 @@ public class AdminBooks_jsp extends HttpJspBase {
 
 // \zxz\
 //  
-//   Filename: Common.jsp/ test/change
+//   Filename: Common.jsp/test/
 //   Generated with CodeCharge  v.1.2.0
 //   JSP.ccp build 05/21/2001
 //
